76cf9bd44112206e0fe5ff46ba1ae6c5a7998462
TBS96 <9tbs6@proton.me>
2024-10-20 20:25:43 +0530

    call/import ThemeProvider in App.jsx and wrap all contents of App.jsx in ThemeProvider with destructured props as its values
    Signed-off-by: TBS96 <9tbs6@proton.me>

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { ThemeProvider } from './contexts/Theme'
 
 const App = () => {
   return (
-    <div className='grid place-items-center w-full h-screen'>
-      Hello
-    </div>
+    <ThemeProvider value={{themeMode, lighttheme, darktheme,}}>
+      <div className='grid place-items-center w-full h-screen'>
+        Hello
+      </div>
+    </ThemeProvider>
   )
 }
 
